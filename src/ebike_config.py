c372e2a2fbeeb1c22dde067913f1fa04ab356994
sot9363 <si9363@mci4me.at>
2026-07-10 20:02:56 +0200

    feat: datenklasse der ebike-konfiguration (laut angabe)

---
+++ src/ebike_config.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class EbikeConfig:
+    """Datenklasse der Ebike- und Fahrerparameter aus der Angabe"""
+
+    rider_mass: float = 70.0
+
+    bike_mass: float = 10.0
+
+    cw_A: float = 0.5625, #Produkt von cw-Wert und Stirnfläche (m^2)
+
+    diameter: float = 27 * 0.0254 #27 Zoll Rad in m,
+
+    radius: float = diameter/2
+
+    motor_constant: float = 1.5 #Motorkonstante (Formelzeichem Km)
+    
+
+if __name__ == "__main__":
+    config = EbikeConfig
+    
+    print(config)
+    print(f"Gesamtmasse: {config.rider_mass + config.bike_mass} kg")
+    print(f"Masse von: Fahrer -> {config.rider_mass} kg, Ebike -> {config.bike_mass} kg")
+    print(f"Raddurchmesser: {config.diameter} m")
